9bec059877a069b7500be7d8fde184c52b7c2a1e
Dave Foley <davidmfoley@gmail.com>
2014-02-28 10:20:14 -0800

    fix formatting

--- index.js
+++ index.js
@@ -3,87 +3,86 @@ var yargs = require('yargs');
 var _ = require('lodash');
 
 var opts = yargs
-.options('t', {alias: 'token'})
-.options('s', {alias: 'save-token'})
-.options('o', {alias: 'organization'})
-.usage('Output a CSV-formatted list of the github repos for an organization')
+  .options('t', {alias: 'token'})
+  .options('s', {alias: 'save-token'})
+  .options('o', {alias: 'organization'})
+  .usage('Output a CSV-formatted list of the github repos for an organization')
   .describe({
     t: 'a github api token',
     s: 'save the token locally (to ./.token)',
     o: 'the name of the organization'
   });
 
-  var args = opts.argv;
+var args = opts.argv;
 
-  if (!(args.o || args.t) || args.o === true) {
-    opts.showHelp();
+if (!(args.o || args.t) || args.o === true) {
+  opts.showHelp();
+  process.exit(1);
+}
+
+var token = args.t;
+if (token && args.s) {
+  fs.writeFileSync(__dirname + '/.token', args.t);
+  console.error('wrote token to ' + __dirname + '/.token');
+}
+
+token = token || fs.readFileSync(__dirname + '/.token', 'utf-8').split("\n")[0];
+
+if (!token || token.length == 0) {
+  opts.showHelp();
+  process.exit(1);
+}
+
+if (!args.o)  {
+  process.exit();
+}
+
+var client = require('octonode').client(token);
+var me = client.me()
+var perPage = 100;
+var org = client.org(args.o)
+var page = 1;
+var allRepos = [];
+
+org.repos({page: page, per_page: perPage}, handleRepos);
+
+function handleRepos(err, repos) {
+  if (err) {
+    console.error(err);
     process.exit(1);
+    return;
+  }
+  allRepos = allRepos.concat(repos);
+  if (repos.length == perPage) {
+    page += 1;
+    return org.repos({page: page, per_page: perPage}, handleRepos);
   }
 
-  var token = args.t;
-  if (token && args.s) {
-    fs.writeFileSync(__dirname + '/.token', args.t);
-    console.error('wrote token to ' + __dirname + '/.token');
-  }
+  allRepos.map(handleRepo);
+}
 
-  token = token || fs.readFileSync(__dirname + '/.token', 'utf-8').split("\n")[0];
+function handleRepo(repoData) {
+  var repo = client.repo(repoData.full_name);
 
-  if (!token || token.length == 0) {
-    opts.showHelp();
-    process.exit(1);
-  }
-
-  if (!args.o)  {
-    process.exit();
-  }
-
-  var client = require('octonode').client(token);
-  var me = client.me()
-  var perPage = 100;
-
-
-  var org = client.org(args.o)
-  var page = 1;
-  org.repos({page: page, per_page: perPage}, handleRepos);
-  var allRepos = [];
-
-  function handleRepos(err, repos) {
+  repo.contributors(function(err, contributors){
+    var names;
     if (err) {
-      console.error(err);
-      process.exit(1);
-      return;
+      console.error('error getting contributors for ' + repoData.name , err);
+      names = [];
     }
-    allRepos = allRepos.concat(repos);
-    if (repos.length == perPage) {
-      page += 1;
-      return org.repos({page: page, per_page: perPage}, handleRepos);
+    else {
+      names = _.map((contributors || []).slice(0,10), formatContributor);
     }
+    console.log(repoData.name + ', '
+                + (repoData.private ? 'PRIVATE' : 'PUBLIC') + ', '
+                + (repoData.fork ? 'FORK' : 'SOURCE') + ', '
+                + repoData.created_at + ', '
+                + repoData.pushed_at + ', '
+                + repoData.language + ', '
+                + names.join(', '));
+  });
+}
 
-    allRepos.map(handleRepo);
-  }
-
-  function handleRepo(repoData) {
-    var repo = client.repo(repoData.full_name);
-
-    repo.contributors(function(err, contributors){
-      var names;
-      if (err) {
-        console.error('error getting contributors for ' + repoData.name , err);
-        names = [];
-      }
-      else {
-        names = _.map((contributors || []).slice(0,10), formatContributor);
-      }
-      console.log(repoData.name + ', '
-                  + (repoData.private ? 'PRIVATE' : 'PUBLIC') + ', '
-                  + (repoData.fork ? 'FORK' : 'SOURCE') + ', '
-                  + repoData.created_at + ', '
-                  + repoData.pushed_at + ', '
-                  + repoData.language + ', '
-                  + names.join(', '));
-    });
-  }
-
-  function formatContributor(contributor) {
-    return(contributor.login + ', ' + contributor.contributions);
-  }
+function formatContributor(contributor) {
+  return(contributor.login + ', ' + contributor.contributions);
+}
