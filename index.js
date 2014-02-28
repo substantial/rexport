0cf88bc95efc59d24b0bc67cb31ec69d522ba691
Dave Foley <davidmfoley@gmail.com>
2014-02-28 09:51:12 -0800

    output csv garbage

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ token = token || fs.readFileSync(__dirname + '/.token', 'utf-8').split("\n")[0];
 
 var client = require('octonode').client(token);
 var me = client.me()
+var perPage = 100;
 
 me.orgs(handleOrgs);
 
@@ -28,19 +29,51 @@ function handleOrgs(err, orgs) {
   }
 
   var org = client.org(args.o)
-  org.repos(handleRepos);
-}
+  var page = 1;
+  org.repos({page: page, per_page: perPage}, handleRepos);
+  var allRepos = [];
 
-function handleRepos(err, repos) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-    return;
+  function handleRepos(err, repos) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+      return;
+    }
+    allRepos = allRepos.concat(repos);
+    if (repos.length == perPage) {
+      page += 1;
+      return org.repos({page: page, per_page: perPage}, handleRepos);
+    }
+
+    allRepos.map(handleRepo);
   }
-
-  repos.map(handleRepo);
 }
 
-function handleRepo(repo) {
-  console.log(repo.name);
+function nextPage(){
+}
+
+
+function handleRepo(repoData) {
+  var repo = client.repo(repoData.full_name);
+
+  repo.contributors(function(err, contributors){
+    var names;
+    if (err) {
+      console.error('error getting contributors for ' + repoData.name , err);
+      names = [];
+    }
+    else {
+      names = _.map((contributors || []).slice(0,10), formatContributor);
+    }
+    console.log(repoData.name + ', '
+                + (repoData.fork ? 'FORK' : 'SOURCE') + ', '
+                + repoData.created_at + ', '
+                + repoData.pushed_at + ', '
+                + repoData.language + ', '
+                + names.join(', '));
+  });
+}
+
+function formatContributor(contributor) {
+  return(contributor.login + ', ' + contributor.contributions);
 }
